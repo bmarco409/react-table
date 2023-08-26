9eae6a09fc3692ba698fbba8f520e67c5c747d06
marco.barcellona <marco.barcellona@cloudtec.it>
2023-08-26 09:28:18 +0200

    fix resize column

--- src/App.tsx
+++ src/App.tsx
@@ -68,21 +68,23 @@ function App(): ReactElement {
             headerName: 'id',
             type: 'number',
             width: 200,
+            minWidth: 200
+
         },
         {
             field: 'email',
             headerName: 'email',
             type: 'string',
             sortable: false,
-            width: 200,
-            minWidth: 200
+           // width: 200,
+           // minWidth: 200
         },
         {
             field: 'name',
             headerName: 'name',
             type: 'string',
             sortable: false,
-            width: 200,
+            //width: 200,
         },
         // {
         //     field: 'surname',
